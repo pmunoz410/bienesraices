7455b759e292edf71427bc2f7292bdcb2f282541
Paul Muñoz <paul18a20@gmail.com>
2024-09-03 17:33:14 -0500

    Subiendo cambios dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     eventListener()
+    darkMode()
 })
 
 function eventListener() {
@@ -12,4 +13,30 @@ function navegacionResponsive() {
     const navegacion = document.querySelector('.navegacion')
 
     navegacion.classList.toggle('mostrar')
+}
+
+function darkMode() {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
+
+    // console.log(prefersDarkMode.matches);
+
+    if(prefersDarkMode.matches) {
+        document.body.classList.add('dark-mode')
+    } else {
+        document.body.classList.remove('dark-mode')
+    }
+
+    prefersDarkMode.addEventListener('change', function() {
+        if(prefersDarkMode.matches) {
+            document.body.classList.add('dark-mode')
+        } else {
+            document.body.classList.remove('dark-mode')
+        }
+    })
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton')
+
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode')
+    })
 }
